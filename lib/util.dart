06f427f17ca47fc26a00f61ae6a6ac00919ff68a
kasunSMTK <kasun@softmastergroup.com>
2023-12-08 21:36:05 +0530

    last commit

--- lib/util.dart
+++ lib/util.dart
@@ -5,35 +5,49 @@ class Command{
 
   static final all = [switchOne,switchTwo,switchThree,switchTo,doorlock,switch1,switch2,
                       switch3,dholak,lightOne,lightTwo,lightThree,lightto,firstLight,
-                      secondLight,thirdLight,allLightOn,allLightOff,allSwitchOn,allSwitchOff];
+                      secondLight,thirdLight,allLightOn,allLightOff,allLightOf,allSwitchOn,allSwitchOff,allSwitchOf,
+                      switchFour,lightFour,fourthLight,switch4,forl,doorUnlock,dhoUnlak,dhoUnloc];
 
   static const switchOne = 'light one';
   static const switchTwo = 'light two';
   static const switchTo = 'light to';
   static const switchThree = 'light three';
+  static const switchFour = 'light four';
 
   static const lightOne = 'switch one';
   static const lightTwo = 'switch two';
   static const lightThree = 'switch three';
   static const lightto = 'switch to';
+  static const lightFour = 'switch four';
 
   static const firstLight = 'first light';
   static const secondLight = 'second light';
   static const thirdLight = 'third light';
+  static const fourthLight = 'fourth light';
 
   static const switch1 = 'light 1';
   static const switch2 = 'light 2';
   static const switch3 = 'light 3';
+  static const switch4 = 'light 4';
+
+  static const forl = 'for';
 
   static const allLightOn = 'all light on';
   static const allLightOff = 'all light off';
+  static const allLightOf = 'all light of';
 
   static const allSwitchOn = 'all switch on';
   static const allSwitchOff = 'all switch off';
+  static const allSwitchOf = 'all switch of';
 
   //door lock
   static const doorlock = 'door lock';
   static const dholak = 'dholak';
+  //do unlock  do unloc
+
+  static const doorUnlock = 'door unlock';
+  static const dhoUnlak = 'do unlock';
+  static const dhoUnloc = 'do unloc';
   
 
 }
@@ -66,13 +80,26 @@ class Util{
 
         return body;
     }
-    if(text.contains(Command.allLightOn) || text.contains(Command.allSwitchOn)){
+    if(text.contains(Command.doorUnlock) || text.contains(Command.dhoUnlak) || text.contains(Command.dhoUnloc)){
+        final body =  _getTextAfterCommand(text:text,command:Command.doorUnlock);
+
+        return body;
+    }
+    if(text.contains(Command.allLightOn) || text.contains(Command.allSwitchOn) ){
         final body =  _getTextAfterCommand(text:text,command:Command.allLightOn);
 
         return body;
     }
-    if(text.contains(Command.allLightOff) || text.contains(Command.allSwitchOff)){
+    if(text.contains(Command.allLightOff) || text.contains(Command.allSwitchOff) || text.contains(Command.allSwitchOf)
+      || text.contains(Command.allLightOf)){
         final body =  _getTextAfterCommand(text:text,command:Command.allLightOff);
+
+        return body;
+    }
+    if(text.contains(Command.switchFour) || text.contains(Command.lightFour) 
+      || text.contains(Command.fourthLight) || text.contains(Command.switch4) 
+      || text.contains(Command.forl)){
+        final body =  _getTextAfterCommand(text:text,command:Command.switchFour);
 
         return body;
     }
--- lib/voiceControl.dart
+++ lib/voiceControl.dart
@@ -13,7 +13,6 @@ class VoiceControl extends StatefulWidget {
 }
 
 class _VoiceControlState extends State<VoiceControl> {
-
   bool isListening = false;
   String text = 'I can hear you';
   // Track the state of each light button
@@ -21,17 +20,13 @@ class _VoiceControlState extends State<VoiceControl> {
   bool light1On = false;
   bool light2On = false;
   bool light3On = false;
+  bool light4On = false;
 
-  
- 
- // Track the state of the door lock
+  // Track the state of the door lock
   bool doorLockOn = false;
 
-   final String esp32IpAddress =
-      '192.168.1.100'; 
+  final String esp32IpAddress = '192.168.1.100';
 
-
-  
   Future<void> _sendCommand(String pin, String action) async {
     print('Sending command - Pin: $pin, Action: $action');
 
@@ -49,81 +44,112 @@ class _VoiceControlState extends State<VoiceControl> {
 
   void _toggleLight(String lightNumber) {
     if (lightNumber == Command.switchOne) {
-
-        light1On = !light1On;
-      _sendCommand('26', light1On ? 'on' : 'off');
+      light1On = !light1On;
+      _sendCommand('12', light1On ? 'off' : 'on');
     } else if (lightNumber == Command.switchTwo) {
-
-        light2On = !light2On;
-      _sendCommand('27', light2On ? 'on' : 'off');
+      light2On = !light2On;
+      _sendCommand('14', light2On ? 'off' : 'on');
     } else if (lightNumber == Command.switchThree) {
+      light3On = !light3On;
+      _sendCommand('26', light3On ? 'off' : 'on');
+    }else if (lightNumber == Command.switchFour) {
+      light4On = !light4On;
+      _sendCommand('27', light4On ? 'off' : 'on');
+    } else if (lightNumber == Command.doorlock) {
+    
+      if (!doorLockOn) {
+        doorLockOn = true;
+        _sendCommand('19', doorLockOn ? 'low' : 'high');
+        Future.delayed(const Duration(seconds: 2), () {
+          _sendCommand('19', 'low');
+        });
+      }
+      
+    } else if (lightNumber == Command.doorUnlock) {
+      
+      if (doorLockOn) {
+         doorLockOn = false;
+        _sendCommand('18', doorLockOn ? 'high' : 'low');
+        Future.delayed(const Duration(seconds: 2), () {
+          _sendCommand('18', 'low');
+        });
+      }
+      
+    }  
+    
+    else if (lightNumber == Command.allLightOn) {
+      // Future.delayed(const Duration(seconds: 1),(){   dan
+      if (!light1On) {
+        light1On = true;
+        _sendCommand('12', light1On ? 'off' : 'on');
+      }
+      //  });
+      // Future.delayed(const Duration(seconds: 1),(){
+      if (!light2On) {
+        light2On = true;
+        _sendCommand('14', light2On ? 'off' : 'on');
+      }
+      // });
+      // Future.delayed(const Duration(seconds: 1),(){
+      if (!light3On) {
+        light3On = true;
+        _sendCommand('26', light3On ? 'off' : 'on');
+      }
 
-       light3On = !light3On;
-      _sendCommand('14', light3On ? 'on' : 'off');
-    }
-    else if (lightNumber == Command.doorlock) {
+      if (!light4On) {
+        light4On = true;
+        _sendCommand('27', light4On ? 'off' : 'on');
+      }
+      // });
+    } else if (lightNumber == Command.allLightOff) {
+      if (light1On) {
+        light1On = false;
+        _sendCommand('12', light1On ? 'off' : 'on');
+      }
 
-       doorLockOn = !doorLockOn;
-      _sendCommand('12', doorLockOn ? 'high' : 'low');
+      if (light2On) {
+        light2On = false;
+        _sendCommand('14', light2On ? 'off' : 'on');
+      }
+
+      if (light3On) {
+        light3On = false;
+        _sendCommand('26', light3On ? 'off' : 'on');
+      }
+
+      if (light4On) {
+        light4On = false;
+        _sendCommand('27', light4On ? 'off' : 'on');
+      }
     }
-    else if(lightNumber == Command.allLightOn){
-  
-     // Future.delayed(const Duration(seconds: 1),(){
-        if(!light1On){light1On = true;_sendCommand('26', light1On ? 'on' : 'off');}
-    //  });
-     // Future.delayed(const Duration(seconds: 1),(){
-        if(!light2On){light2On = true;_sendCommand('27', light2On ? 'on' : 'off');}
-     // });
-     // Future.delayed(const Duration(seconds: 1),(){
-        if(!light3On){light3On = true;_sendCommand('14', light3On ? 'on' : 'off');}
-     // });
-      
-    }
-    else if(lightNumber == Command.allLightOff){
-     
-        if(light1On){light1On = false;_sendCommand('26', light1On ? 'on' : 'off');}
-      
-      
-        if(light2On){light2On = false;_sendCommand('27', light2On ? 'on' : 'off');}
-      
-      
-        if(light3On){light3On = false;_sendCommand('14', light3On ? 'on' : 'off');}
-      
-    }
-    print(lightNumber+" "+Command.allLightOn);
+    
+    //  print(lightNumber + " " + Command.allLightOn);
   }
-
-  
- 
 
   @override
   void initState() {
     super.initState();
-  
   }
 
-
-Future toggleRecoding() => SpeachApi.toggleRecoding(
-      onResult: (text) => setState(()=>this.text =text),
-      onListning: (isListening) { 
-        setState(() {
-          this.isListening = isListening;
-
-          if(!isListening){
-            print("finshed lisning");
-           // Future.delayed(const Duration(seconds: 1),(){
-             String captureText =  Util.scanText(text);
+  Future toggleRecoding() => SpeachApi.toggleRecoding(
+        onResult: (text) => setState(() => this.text = text),
+        onListning: (isListening) {
+          setState(() {
+            this.isListening = isListening;
+            print("kkkkkkkkk");
+            if (!isListening) {
+              print("finshed lisning");
+              // Future.delayed(const Duration(seconds: 1),(){
+              String captureText = Util.scanText(text);
 
               print(text);
-             _toggleLight(captureText);
-             
-           // });
-              
-          }
-        });
-       },
-      );
+              _toggleLight(captureText);
 
+              // });
+            }
+          });
+        },
+      );
 
   @override
   Widget build(BuildContext context) {
@@ -200,6 +226,27 @@ Future toggleRecoding() => SpeachApi.toggleRecoding(
                   ),
                 ),
                 SizedBox(height: 20.0),
+                                const Text(
+                  "Light 4",
+                  style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+                ),
+                IgnorePointer(
+                  ignoring: true,
+                  child: FlutterSwitch(
+                    value: light4On,
+                    onToggle: (value) {
+                      //_toggleLight(lightNumber);
+                    },
+                    activeText: 'On',
+                    inactiveText: 'Off',
+                    activeColor: Colors.green,
+                    inactiveColor: Colors.grey,
+                    toggleColor: Colors.white,
+                    width: 100.0,
+                    height: 40.0,
+                  ),
+                ),
+                SizedBox(height: 20.0),
                 const Text(
                   "Door Lock",
                   style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
@@ -230,36 +277,33 @@ Future toggleRecoding() => SpeachApi.toggleRecoding(
               child: Center(
                 child: SubstringHighlight(
                   text: text,
-                  terms:Command.all,
+                  terms: Command.all,
                   textStyle: const TextStyle(
-                    fontSize: 30,
-                    color: Colors.black,
-                    fontWeight: FontWeight.w300
-                  ),
+                      fontSize: 30,
+                      color: Colors.black,
+                      fontWeight: FontWeight.w300),
                   textStyleHighlight: const TextStyle(
-                    fontSize: 32,
-                    color: Colors.red,
-                    fontWeight: FontWeight.w400
-                  ),
-                  ),
-                
+                      fontSize: 32,
+                      color: Colors.red,
+                      fontWeight: FontWeight.w400),
+                ),
               ),
             ),
           ],
         ),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      floatingActionButton:  AvatarGlow(
+      floatingActionButton: AvatarGlow(
         animate: isListening,
         endRadius: 75,
         glowColor: Theme.of(context).primaryColor,
         child: FloatingActionButton(
-      
-          onPressed: toggleRecoding,
-          child: Icon(isListening ? Icons.mic:Icons.mic_none,size: 36,)),
+            onPressed: toggleRecoding,
+            child: Icon(
+              isListening ? Icons.mic : Icons.mic_none,
+              size: 36,
+            )),
       ),
     );
   }
-
-
 }
